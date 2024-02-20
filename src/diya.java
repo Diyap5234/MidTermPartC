be202ca4bdf6a6986a3b145825c8cf367a1ee612
Diya Patel <dpatel34922004@gmail.com>
2024-02-20 12:34:29 -0500

    created a branch and a java class

---
+++ src/diya.java
@@ -0,0 +1,12 @@
+/* created a branch on 20 feb 2024 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dpate
+ */
+public class diya {
+    
+}
